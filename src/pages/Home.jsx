21198855ac5e6dfa819f94a887954255eb3527e0
Joe <chanmacasa@gmail.com>
2025-08-11 21:30:51 +0800

    add home and not found pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () =>{
+    return <div>
+        {/*Theme Toggle*/}
+
+        {/*Background Effects*/}
+
+        {/*Navbar*/}
+
+        {/*Main Content*/}
+
+        {/*Footer*/}
+
+    </div>
+}
