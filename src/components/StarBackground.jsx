57e1f2bd57076406e9a152d4622196c0c31d78c9
Joe <chanmacasa@gmail.com>
2025-08-19 19:25:26 +0800

    Adding resizehandler

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -7,6 +7,13 @@ export const StarBackground =() =>{
     useEffect(()=>{
         generateStars();
         generateMeteors();
+
+        const handleResize = () =>{
+          generateStars();  
+        };
+
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener("resize", handleResize)
     }, [])
 
     const generateStars = () =>{
@@ -63,8 +70,8 @@ export const StarBackground =() =>{
         {meteors.map((meteor)=>(
             <div key={meteor.id} 
             className="meteor animate-meteor" style={{
-                width: meteor.size + "px",
-                height: meteor.size + "px",
+                width: meteor.size * 15 + "px",
+                height: meteor.size  * 2+ "px",
                 left: meteor.x + "%",
                 top: meteor.y + "%",
                 animationDelay: meteor.delay,
