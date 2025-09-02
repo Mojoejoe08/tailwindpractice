2c75a3197109d0a03525d374b1e177b2c51fa156
Joe <chanmacasa@gmail.com>
2025-09-02 20:59:47 +0800

    add tag, description of project and github button

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
 
 const projects = [
@@ -39,13 +40,37 @@ export const ProjectSection = () => {
             </p>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                {projects.map((project, key) =>(
+                {projects.map((project, key) => (
                     <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
                         <div className="h-48 overflow-hidden">
-                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+                        </div>
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-primary/20 text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+
+                            <h3 className="text-md font-semibold mb-1">{project.title}</h3>
+                            <p className="text-muted-foreground text-sm ">{project.description}</p>
+                            <div className="flex justify-between item-center">
+                                <div className="flex space-x-3 mt-2 mb-2">
+                                    <a href={project.demoURL} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300 "><ExternalLink size={20} /></a>
+                                    <a href={project.githubUrl} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300 "><Github size={20} /></a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="text-center mt-12">
+                <a className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                    href="https://github.com/Mojoejoe08" target="_blank">
+                    Check My Github <ArrowRight size={16} />
+                </a>
             </div>
         </div>
     </section>;
